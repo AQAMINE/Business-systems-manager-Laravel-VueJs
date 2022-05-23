0d3fb721c9f0437406c0c140f1a6347e81ccdcbb
amine aqebli <aqebli36@gmail.com>
2022-05-23 01:07:32 +0100

    TestTokenFetch

--- BSM-FE/src/views/DashboardPage.vue
+++ BSM-FE/src/views/DashboardPage.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <analytical-card title="Users registered" number="22" change="+4" logo="fa-users"></analytical-card>
     <base-card title="This the base card">hello </base-card>
+    <button @click="testToken">ClickMe</button>
   </div>
 </template>
 
@@ -10,6 +11,18 @@
 
 export default {
   name: 'Home',
- 
+  data(){
+    return {
+      
+    }
+  },
+  methods:{
+    testToken(){
+      console.log(this.$config.app_url + '/api/v1/tasks');
+      window.axios.get(this.$config.app_url + '/api/v1/tasks').then(response  => {
+        console.log(response.data);
+      })
+    }
+  }
 }
 </script>
